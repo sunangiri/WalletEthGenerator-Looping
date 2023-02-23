2e102da3c6799bde3895ade3e6a880063b905fa9
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-02-24 06:30:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const { ethers } = require("ethers");
     const wallet = ethers.Wallet.createRandom();
 
     // Definisikan provider
-    const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/b5838b5679c54cacb756af57c51cc5e7");
+   // const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/b5838b5679c54cacb756af57c51cc5e7");
 
     // melihat saldo/balance dari address
-    const balance = await provider.getBalance(wallet.address);
+    //const balance = await provider.getBalance(wallet.address);
     // mengubah nilai Ether dalam satuan wei menjadi nilai Ether dalam satuan ether. (10^18)
-    const eth = ethers.utils.formatEther(balance);
+    //const eth = ethers.utils.formatEther(balance);
 
     // Output print
     console.log("Address: " + wallet.address);
     console.log("Public Key: " + wallet.publicKey);
     console.log("Private Key:" + wallet.privateKey);
-    console.log(`Balance: ${eth} ETH`);
+   // console.log(`Balance: ${eth} ETH`);
     console.log("=====================================================");
 
     // Jika balance nilainya selain 0, maka looping berhenti
-    if (eth != 0) break;
+    //if (eth != 0) break;
   }
 })();
